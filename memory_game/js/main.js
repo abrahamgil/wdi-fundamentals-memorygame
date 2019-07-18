50eeeea64adb634caceb6cdbae3d7da445ff521a
Abraham <abrahamgil2@gmail.com>
2019-07-17 22:09:23 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,14 +2,15 @@ var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-var cardTwo = cards[2];
+function checkForMatch() {
+	if (cardsInPlay[0] == cardsInPlay[1]) {
+	alert("You found a match! " + cardsInPlay[0] + " " + cardsInPlay[1]);
+	} else {
+		alert("You have not found a match! " + cardsInPlay[0] + " " + cardsInPlay[1]);
+	}
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
+};
 
-console.log("User flipped " + cardsInPlay[0]);
-console.log("User flipped " + cardsInPlay[1]);
 
 if (cardsInPlay.length === 2) {
 	console.log("There are 2 cards in play.");
@@ -17,8 +18,11 @@ if (cardsInPlay.length === 2) {
 	console.log("There are " + cardsInPlay.length + " in play.");
 }
 
-if (cardsInPlay[0] == cardsInPlay[1]) {
-	alert("You found a match! " + cardsInPlay[0] + " " + cardsInPlay[1]);
-} else {
-	alert("You have not found a match! " + cardsInPlay[0] + " " + cardsInPlay[1]);
-}
+
+function flipCard(cardId) {
+	var card = cards[cardId];
+	cardsInPlay.push(card);
+	console.log("User flipped " + card);
+	checkForMatch(card);
+
+};
